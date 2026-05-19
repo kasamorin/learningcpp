d168431b4872bf76bf19ffd8c8ec7509d58b5f2e
morin <kasamorin@outlook.com>
2026-05-19 23:25:42 +0800

    learn ifdef&ifndef&endif

--- moreFiles/add.cpp
+++ moreFiles/add.cpp
@@ -1,4 +1,16 @@
+#include<iostream>
+#define DONGBEI
+
+#ifdef DONGBEI //从这里开始编译
 int add(int x,int y)
 {
     return x + y;
 }
+#endif //到这里结束编译
+
+#ifndef YUJIE // 从这里开始不编译
+int test(int x,int y)
+{
+    return x * y;
+}
+#endif // 到这里结束不编译
